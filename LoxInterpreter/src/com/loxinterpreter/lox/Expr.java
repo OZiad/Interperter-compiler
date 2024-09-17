2b34db5d78a8052cbbd64744a09935e28b9b6f28
Omar Ziad <o.ziad@outlook.com>
2024-09-16 22:25:44 -0400

    Move attributes above constructors

--- LoxInterpreter/src/com/loxinterpreter/lox/Expr.java
+++ LoxInterpreter/src/com/loxinterpreter/lox/Expr.java
@@ -2,40 +2,40 @@ package com.loxinterpreter.lox;
 
 abstract class Expr {
     static class Binary extends Expr {
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
         }
-
-        final Expr left;
-        final Token operator;
-        final Expr right;
     }
 
     static class Grouping extends Expr {
+        final Expr expression;
+
         Grouping(Expr expression) {
             this.expression = expression;
         }
-
-        final Expr expression;
     }
 
     static class Literal extends Expr {
+        final Object value;
+
         Literal(Object value) {
             this.value = value;
         }
-
-        final Object value;
     }
 
     static class Unary extends Expr {
+        final Token operator;
+        final Expr right;
+
         Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
         }
-
-        final Token operator;
-        final Expr right;
     }
 }
